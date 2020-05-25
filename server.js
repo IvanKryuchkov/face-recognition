14857ab268b42051df734e46543c0fb88e3d7ed9
IvanKryuchkov <55559380+IvanKryuchkov@users.noreply.github.com>
2020-05-26 01:10:11 +0300

    adding port

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send('its workin ayoooo'/*database.users*/) })
 
 app.post('/signin', signin.handleSignIn(db, bcrypt))
 
